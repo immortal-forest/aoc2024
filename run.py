2e0df8790041f5c4892abea04131c3c966d40684
immortal-forest <ali.salman.sa.01@gmail.com>
2024-12-02 12:47:28 +0545

    feat: calc time taken

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import importlib
 import sys
+from time import time
 
 
 def main(args: list):
@@ -30,17 +31,29 @@ def main(args: list):
     match part:
         case 0:
             print("Part 1:", end="\n\t")
+            s1 = time()
             module.part1()
+            e1 = time()
+            print(f"\tTook: {e1-s1}")
             print("Part 2:", end="\n\t")
+            s2 = time()
             module.part2()
+            e2 = time()
+            print(f"\tTook: {e2-s2}")
 
         case 1:
             print("Part 1:", end="\n\t")
+            s1 = time()
             module.part1()
+            e1 = time()
+            print(f"\tTook: {e1-s1}")
 
         case 2:
             print("Part 2:", end="\n\t")
+            s2 = time()
             module.part2()
+            e2 = time()
+            print(f"\tTook: {e2-s2}")
     print("\n" + "=" * 50)
 
 
